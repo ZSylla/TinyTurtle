0b550e539742f82c696cadff00342319987a8ffa
Zenebou Sylla <zsylla@ymail.com>
2017-03-03 16:57:17 +0000

    House

---
+++ script.js
@@ -0,0 +1,15 @@
+TinyTurtle.apply(window); 
+
+forward(50);
+left(90);
+forward(45);
+left(90);
+forward(50);
+left(90);
+forward(45);
+left(90);
+forward(50);
+left(45);
+forward(32);
+left (90);
+forward(32);
